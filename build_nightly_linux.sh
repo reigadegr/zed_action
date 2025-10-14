14c44660fae243ba6971d55b84f113ecae7b7045
reigadegr <2722688642@qq.com>
2025-10-14 13:36:04 +0800

    chore: 关闭一些flag

--- build_nightly_linux.sh
+++ build_nightly_linux.sh
@@ -42,4 +42,4 @@ export CARGO_TERM_COLOR=always
 
 export JEMALLOC_SYS_DISABLE_WARN_ERROR=1
 
-cargo +nightly build -r --target "$1" -Z trim-paths -Z build-std=core,alloc,std,panic_abort -Z build-std-features="compiler-builtins-mem" --all-features
+cargo +nightly build -r --target "$1" -Z trim-paths -Z build-std=core,alloc,std,panic_abort --all-features
