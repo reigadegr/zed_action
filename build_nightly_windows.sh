715a4b62f3849a64c194c8bc14fd5ce10e176b90
reigadegr <2722688642@qq.com>
2025-10-14 14:23:36 +0800

    feat: win编译时也build-std

--- build_nightly_windows.sh
+++ build_nightly_windows.sh
@@ -40,4 +40,4 @@ export CARGO_TERM_COLOR=always
 
 export JEMALLOC_SYS_DISABLE_WARN_ERROR=1
 
-cargo +nightly build -r --target "$1" -Z trim-paths --all-features
+cargo +nightly build -r --target "$1" -Z trim-paths -Z build-std=core,alloc,std,panic_abort --all-features
